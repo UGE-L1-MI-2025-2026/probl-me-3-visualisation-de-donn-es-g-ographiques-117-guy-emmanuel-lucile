9c780b23e7338df9b49d3f6665d85cb41b8af0e2
emm <wolrusteen@ya.ru>
2025-11-24 14:42:26 +0100

    ajoute fltk.py

--- pyshp reader.py
+++ pyshp reader.py
@@ -5,8 +5,10 @@ path = os.getcwd()
 fichier_texte = (
     path + "/departements-20180101-shp/departements-20180101.shp"
 )
-shape = shapefile.Reader(fichier_texte)
-#first feature of the shapefile
-feature = shape.shapeRecords()[0]
-first = feature.shape.__geo_interface__
-print(first) # (GeoJSON format)
+sf = shapefile.Reader(fichier_texte)
+records = sf.records()
+print(records)
+seine_et_marne = sf.shape(47)
+print(seine_et_marne.bbox)
+print(seine_et_marne.points)
+
